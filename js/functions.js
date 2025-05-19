12fcbae8b463e2c69d7a34e590b3d002b68b638f
Shaiyaan Ishtiaq <16546987+menamejeff911@users.noreply.github.com>
2025-05-19 02:56:56 -0400

    changes

--- js/functions.js
+++ js/functions.js
@@ -92,27 +92,33 @@ function startHeartAnimation() {
 	};
 })(jQuery);
 
-function timeElapse(date){
-	var current = Date();
+function timeElapse(date) {
+	var current = new Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+
+	var daysTotal = Math.floor(seconds / (3600 * 24));
+	var years = Math.floor(daysTotal / 365);
+	var days = daysTotal % 365;
+
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
-	if (hours < 10) {
-		hours = "0" + hours;
-	}
 	seconds = seconds % 3600;
 	var minutes = Math.floor(seconds / 60);
-	if (minutes < 10) {
-		minutes = "0" + minutes;
-	}
-	seconds = seconds % 60;
-	if (seconds < 10) {
-		seconds = "0" + seconds;
-	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	seconds = Math.floor(seconds % 60);
+
+	if (hours < 10) hours = "0" + hours;
+	if (minutes < 10) minutes = "0" + minutes;
+	if (seconds < 10) seconds = "0" + seconds;
+
+	var result = "<span class='digit'>" + years + "</span> years " +
+	             "<span class='digit'>" + days + "</span> days " +
+	             "<span class='digit'>" + hours + "</span> hours " +
+	             "<span class='digit'>" + minutes + "</span> minutes " +
+	             "<span class='digit'>" + seconds + "</span> seconds";
+
 	$("#elapseClock").html(result);
 }
+
 
 function showMessages() {
 	adjustWordsPosition();
